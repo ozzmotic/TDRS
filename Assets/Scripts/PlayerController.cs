a567b170d0f16e2a6f03afca5834eb9ff3d27c0a
Alex_A <ozzmotic@gmail.com>
2014-03-04 20:25:34 -0800

    gamepad support for movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,25 +8,54 @@ public class PlayerController : MonoBehaviour {
 	public float rotationSpeed = 450f;
 	public float speed = 10;
 	public float gravity = 20.0f;
+	private float deltaX;
+	private float deltaY;
 
 	//Components
 	private CharacterController controller;
 
 	//System
 	private Vector3 moveDirection = Vector3.zero;
+	private bool gamePad;
 
 	void Start () {
 		controller = GetComponent<CharacterController>();
+
+		if (Input.GetJoystickNames().Length == 0) {
+			gamePad = false;
+		} else {
+			gamePad = true;
+		}
 	}
 	
 	void Update () {
-		moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
-		var v3 = Input.mousePosition;
-		v3.z = 25.0f;
-		v3 = Camera.main.ScreenToWorldPoint(v3);
 
-		Vector3 lookPos = new Vector3(v3.x,transform.position.y,v3.z);
-		transform.LookAt(lookPos);
+		if (gamePad) {
+			moveDirection = new Vector3(Input.GetAxis("GamepadMoveHorizontal"),0,Input.GetAxis("GamepadMoveVertical"));
+
+
+			deltaX = Input.GetAxis("GamepadAimHorizontal");
+			deltaY = Input.GetAxis("GamepadAimVertical");
+
+			float angle = Mathf.Atan2(deltaY,deltaX);
+
+			if (deltaX != 0 && deltaY != 0) {
+				angle = angle * 180/Mathf.PI + 90;
+				Vector3 targetRotation = new Vector3(0,angle,0);
+				Debug.Log (angle);
+				transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(targetRotation), 3f);
+			}
+
+		} else {
+			moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+
+			var v3 = Input.mousePosition;
+			v3.z = 25.0f;
+			v3 = Camera.main.ScreenToWorldPoint(v3);
+			
+			Vector3 lookPos = new Vector3(v3.x,transform.position.y,v3.z);
+			transform.LookAt(lookPos);
+		}
 
 		moveDirection *= speed;
 
